13ea100754c34eeb0a8c07719f352df7a037ed22
ssbright <ssbright11@gmail.com>
2023-05-04 15:53:34 -0700

    working connect to nostr

--- chatbtc-nostr-ts/src/index.ts
+++ chatbtc-nostr-ts/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+// Import the package
+import NDK from "@nostr-dev-kit/ndk";
+
+async function main() {
+  const ndk = new NDK({ explicitRelayUrls: ["wss://relay.house", "wss://relay.damus.io"] });
+  await ndk.connect();
+
+  console.log('Hello, world!');
+}
+
+main();
+
+
+
