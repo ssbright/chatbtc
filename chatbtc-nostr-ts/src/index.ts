36e7af4361558ac8686382a18f75885abeb197f6
ssbright <ssbright11@gmail.com>
2023-05-09 16:09:42 -0700

    working publishing of text

--- chatbtc-nostr-ts/src/index.ts
+++ chatbtc-nostr-ts/src/index.ts
@@ -1,11 +1,25 @@
 // Import the package
-import NDK from "@nostr-dev-kit/ndk";
+import NDK, { NDKPrivateKeySigner, NDKEvent } from "@nostr-dev-kit/ndk";
+import {config} from "dotenv";
+import * as path from 'path';
+
+const envPath = path.join(__dirname, '../..', '.env');
+config({ path: envPath });
 
 async function main() {
-  const ndk = new NDK({ explicitRelayUrls: ["wss://relay.house", "wss://relay.damus.io"] });
+    const pubkey = process.env.PUBKEY;
+    const privkey = process.env.PRIVKEY;
+
+
+  const ndksigner = new NDKPrivateKeySigner(privkey);
+  const ndk = new NDK({ explicitRelayUrls: ["wss://relay.house", "wss://relay.damus.io"], signer: ndksigner });
   await ndk.connect();
 
-  console.log('Hello, world!');
+
+  const ndkEvent = new NDKEvent(ndk);
+  ndkEvent.kind = 1;
+  ndkEvent.content = "Hello again, world!";
+  ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.
 }
 
 main();
